ae54e6bf3425a2b3fdbf4042d79a00d981b5c8ff
pnispero <pnispero@slac.stanford.edu>
2024-06-03 13:42:08 -0700

    completing create branch

--- bs_cli/component.py
+++ bs_cli/component.py
@@ -1,29 +1,59 @@
 import os
-import git
+from git import Repo, exc
 from auto_complete import AutoComplete
 
 class Component(object):
     def __init__(self, name: str=None, branch_name: str=None):
         self.name = name
         self.branch_name = branch_name
+        self.git_repo = self.get_git_root()
 
-    def get_git_root(self, path):
+    def get_git_root(self):
+        # Note - using git_repo.git.<action> allows usage of git like command line git
         try:
-            git_repo = git.Repo(path, search_parent_directories=True)
+            git_repo = Repo(os.getcwd(), search_parent_directories=True)
             return git_repo
         except:
             return None
+        
+    def git_create_branch(self, branch_point_type, branch_name):
+        # Create the branch
+        self.git_repo.git.checkout('-b', branch_name)  # Create a new branch.
+        # if (branch_point_type == 'branch'):
+        #     self.git_repo.create_head()
+        #     self.git_repo.checkout(b=)
+        # elif (branch_point_type == 'tag'):
+        #     git.checkout("")
+        # elif (branch_point_type == 'commit'):
+        #     git.checkout("")
+
+    def git_commit(self, author):
+        # Commit empty
+        self.git_repo.git.commit("--allow-empty", '-m', 'initial commit')
+
+    def git_push(self, branch_name):
+        # Commit and push to remote
+        try:
+            self.git_repo.git.push('-u', 'origin', branch_name)
+        except exc.GitError as error:
+            print(error)
+            print("Deleting branch...")
+            self.git_repo.git.checkout(self.branch_name)
+            self.git_repo.git.branch('-D', branch_name)
+        # TODO: may delete local branch if fail to push
+
+        # origin = self.git_repo.remote()
+        # origin.push()
 
     def set_cur_dir_component(self):
         print("Checking current directory if a component...")
         # Safe to assume that if the current directory is a git repo 
         # then that repo is a component in the component db
-        git_repo = self.get_git_root(os.getcwd())
-        if (git_repo == None):
+        if (self.git_repo == None):
             return False
-        git_root = git_repo.git.rev_parse("--show-toplevel")
+        git_root = self.git_repo.git.rev_parse("--show-toplevel")
         repo_name = os.path.splitext(os.path.basename(git_root))[0]
-        branch_name = git_repo.active_branch.name
+        branch_name = self.git_repo.active_branch.name
         self.name = repo_name
         self.branch_name = branch_name
         return True
@@ -32,7 +62,7 @@ class Component(object):
         AutoComplete.set_auto_complete_vals("component")
         if (not prompt):
             prompt = 'What is the component name? (<tab>-complete) '
-        self.name = input('What is the component name? (<tab>-complete) ')
+        self.name = input(prompt)
 
     def prompt_branch_name(self, prompt: str=None):
         AutoComplete.set_auto_complete_vals("branch")
@@ -40,7 +70,8 @@ class Component(object):
             prompt = 'What is the branch name? (<tab>-complete) '
         self.branch_name = input(prompt)
 
-    def set_component_field(self, field: str):
+    # logic does 3 seperate steps to set a field
+    def set_component_field_logic(self, field: str):
         if (field == "name"):
             # 1) If component option passed in, then use that
             if (self.name):
--- bs_cli/create_commands.py
+++ bs_cli/create_commands.py
@@ -1,7 +1,10 @@
 import click
+import git
 from request import Request
 from component import Component
 import logging
+import inquirer
+
 
 @click.group()
 def create():
@@ -35,33 +38,81 @@ def repo(component: str, branch: str, url: str):
 @click.option("-ft", "--feat", type=int, required=False, help="Add feature branch based off issue number")
 @click.option("-d", "--dev", required=False, help="Add development branch")
 @click.option("-b", "--branch", required=False, help="Specify which branch to branch from")
-def branch(component: str, fix: int, feat: int, dev: str, branch: str):
+@click.option("-t", "--tag", required=False, help="Specify which tag to branch from")
+@click.option("-ct", "--commit", required=False, help="Specify which tag to branch from")
+def branch(component: str, fix: int, feat: int, dev: str, branch: str, tag: str, commit: str):
     """Create a new branch"""
-    request = Request(Component(component))
+    component_obj = Component(component)
+    request = Request(component_obj)
     # TODO: May make the branch ourselves in this case not through backend
     # TODO: Force the user to enter the repository they want to create a branch in
     #       This will make it simpler to use git commands directly
     
-    dont do same logic as component where you look at what branch their sitting in
-    Just have option available or prompt user. 
-    We also want to create a branch off a branch (tip of branch),
-    tag (may want to add sanity check logic if there are newer tags
-          than the user wants to branch off from),
-    or committ.
+    # dont do same logic as component where you look at what branch their sitting in
+    # Just have option available or prompt user. 
+    # We also want to create a branch off a 
+    # branch (tip of branch),
+    # tag (may want to add sanity check logic if there are newer tags
+    #       than the user wants to branch off from),
+    # or committ.
     
-    branch_name=''
-    request.component.prompt_branch_name('Branch name to branch from? (<tab>-complete) ')
-    # Commands to use
-    # git commit --allow-empty -m "initial commit"
-    # git push -u origin <branch_name>
+    # 1) First check that user is in repo
+    if (not component_obj.set_cur_dir_component()):
+        click.echo('fatal: not a git repository (or any of the parent directories)')
+        return
+    
+    # TODO: Create logic to check if branch/tag/commit that user entered is valid, otherwise prompt
+    # 2) See if branch, tag, committ option filled out, or prompt user
+    if (branch): 
+        branch_point_type = 'branch'
+        branch_point_value = branch
+    elif (tag): 
+        branch_point_type = 'tag'
+        branch_point_value = tag
+    elif (commit): 
+        branch_point_type = 'commit'
+        branch_point_value = commit
+    else:
+        questions = [inquirer.List(
+                    "branch_point",
+                    message="Specify what to branch from",
+                    choices=["branch", "tag", "commit"],)]
+        branch_point_type = inquirer.prompt(questions)['branch_point']
+        # TODO: Add logic to tab auto-complete for any of the types
+        branch_point_value = input("Specify name of " + branch_point_type + ": ")
 
+    # 3) See if fix, feat, or dev option filled out, or prompt user
+    if (fix): 
+        branch_type = 'fix'
+        branch_type = fix
+    elif (feat):
+        branch_type = 'feat'
+        branch_type = feat
+    elif (dev):
+        branch_type = 'dev'
+        branch_type = dev
+    else:
+        branch_type = input("Specify type of branch to create (fix | feat | dev): ")
+        branch_type_value = input("Specify name of issue number (or dev name): ")
+
+    full_branch_name = branch_type + '-' + branch_type_value
+
+    # 4) Write to database
     request.set_endpoint('branch')
     request.set_component_name()
+    request.add_to_payload("branch", full_branch_name)
     payload_received = request.post_request()
 
     logging.info(request.headers)
     logging.info(request.payload)
     logging.info(payload_received)
+
+    # 5) Create the branch using git and push
+    component_obj.git_create_branch(branch_point_type, full_branch_name)
+    component_obj.git_commit(request.github_uname)
+    component_obj.git_push(full_branch_name)
+
+
 
 @create.command()
 def issue(): 
