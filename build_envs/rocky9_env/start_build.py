4647890875f0c4ec2b66df042afdfa4b13fc682c
pnispero <pnispero@slac.stanford.edu>
2024-06-05 10:06:16 -0700

    Added test-deployment along with build script that doesn't do much yet except for print out the build-config.json

--- build_envs/rocky9_env/start_build.py
+++ build_envs/rocky9_env/start_build.py
@@ -5,7 +5,10 @@ print('Hello World')
 while True:
     print('Print contents of build_config.json to avoid dying:')
     print(os.getcwd())
-    with open('build_config.json') as f:
+    os.chdir('/etc/build')
+    print(os.getcwd())
+    print(os.listdir(os.getcwd()))
+    with open('/etc/build/build_config.json') as f:
         d = json.load(f)
         print(d)
     time.sleep(10)
