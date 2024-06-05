324ab31ee3bea5168aac98bdc05df2e204013c75
pnispero <pnispero@slac.stanford.edu>
2024-06-05 10:20:55 -0700

    Cleaned up rocky9_env

--- build_envs/rocky9_env/start_build.py
+++ build_envs/rocky9_env/start_build.py
@@ -1,14 +1,8 @@
-import time
 import json
 import os
 print('Hello World')
-while True:
-    print('Print contents of build_config.json to avoid dying:')
-    print(os.getcwd())
-    os.chdir('/etc/build')
-    print(os.getcwd())
-    print(os.listdir(os.getcwd()))
-    with open('/etc/build/build_config.json') as f:
-        d = json.load(f)
-        print(d)
-    time.sleep(10)
+print(os.getcwd())
+print('Print contents of build_config.json:')
+with open('build_config.json') as f:
+    d = json.load(f)
+    print(d)
