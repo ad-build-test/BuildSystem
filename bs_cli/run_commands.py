9c555aec0fad8e604837d3b6ddaf1fed90348b60
pnispero <pnispero@slac.stanford.edu>
2024-06-05 17:04:41 -0700

    semi-complete

--- bs_cli/run_commands.py
+++ bs_cli/run_commands.py
@@ -1,33 +1,47 @@
 import click
-import requests
+import logging
 from component import Component
 from request import Request
-# TODO: remove options from group to each command
 
+# TODO: May make logic a single function since its the same for all 3
+# make the endpoint an argument
 @click.group()
-@click.option("-c", "--component", required=False, help="Component Name")
-@click.option("-b", "--branch", required=False, help="Branch Name")
-@click.pass_context
-def run(ctx, component, branch):
+def run():
     """Run a [ build | deployment | test ]"""
-    component_obj = Component(component, branch)
-    ctx.obj = Payload(component_obj)
     pass
 
 @run.command()
-@click.pass_obj # Grab 'component' object from run group
-def build(payload):
+@click.option("-c", "--component", required=False, help="Component Name")
+@click.option("-b", "--branch", required=False, help="Branch Name")
+def build(component: str, branch: str):
     """Trigger a build"""
-    payload.set_component_fields()
-    print(payload.send_payload)
-    payload_received = requests.post(payload.url + 'build', payload.send_payload)
-    print(payload_received)
+    # 1) Set fields
+    request = Request(Component(component, branch))
+    request.set_component_fields()
+
+    # 2) Write to database
+    endpoint = 'component/' + request.component.name + '/build'
+    request.set_endpoint(endpoint)
+    payload_received = request.post_request()
+
+    logging.info(request.headers)
+    logging.info(request.payload)
+    logging.info(payload_received)
 
 @run.command()
-@click.pass_obj
-def test(payload):
+@click.option("-c", "--component", required=False, help="Component Name")
+@click.option("-b", "--branch", required=False, help="Branch Name")
+def test(component: str, branch: str):
     """Trigger a test"""
-    payload.set_component_fields()
-    print(payload.send_payload)
-    payload_received = requests.post(payload.url + 'test', payload.send_payload)
-    print(payload_received)
+    # 1) Set fields
+    request = Request(Component(component, branch))
+    request.set_component_fields()
+
+    # 2) Write to database
+    endpoint = 'component/' + request.component.name + '/test'
+    request.set_endpoint(endpoint)
+    payload_received = request.post_request()
+
+    logging.info(request.headers)
+    logging.info(request.payload)
+    logging.info(payload_received)
