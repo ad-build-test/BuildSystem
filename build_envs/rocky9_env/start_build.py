8238fd8be0783094dfa768877f3201c0b4da78d7
pnispero <pnispero@slac.stanford.edu>
2024-06-07 10:08:11 -0700

    Organized files

--- build_envs/rocky9_env/start_build.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-print('Hello World')
-print(os.getcwd())
-print('Print contents of build_config.json:')
-with open('build_config.json') as f:
-    d = json.load(f)
-    print(d)
