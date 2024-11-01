f134fade28173cf99d5a5d768b0daebe151c0c1d
pnispero <pnispero@slac.stanford.edu>
2024-11-01 15:32:02 -0700

    Finished rearraging commands, and putting in placeholders for unfinished ones

--- bs_cli/adbs_cli/cli_configuration.py
+++ bs_cli/adbs_cli/cli_configuration.py
@@ -1,5 +1,6 @@
 # Configuration for the cli
 from enum import Enum
+import sys
 
 # Note - this is a copy of ~/.cram_user_facilities.cfg for app type for now
 class Api(str, Enum):
@@ -13,3 +14,8 @@ cli_configuration = {
 # "server_url": "https://accel-webapp-dev.slac.stanford.edu/api/cbs/v1/"
 
 INPUT_PREFIX = "[?] "
+
+def under_development():
+    """ Function placeholder for unfinished logic """
+    print("== This cli command is under development!! ==")
+    sys.exit(0)
