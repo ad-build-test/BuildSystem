5d3cb62fa41748df6faefde6aa81bc01b8e46975
pnispero <pnispero@slac.stanford.edu>
2025-11-13 17:30:06 -0800

    Merged admin commands configure-repo add-repo and add-initial-deployment into onboard-repo to streamline the process, and automated some steps

--- bs_cli/adbs_cli/admin_commands.py
+++ bs_cli/adbs_cli/admin_commands.py
@@ -4,19 +4,213 @@ import re
 import json
 import inquirer
 import os
+import subprocess
 from adbs_cli.request import Request
 from adbs_cli.component import Component
 from adbs_cli.auto_complete import AutoComplete
 from adbs_cli.cli_configuration import cli_configuration, Api, ApiEndpoints, INPUT_PREFIX
+from adbs_cli.create_commands import branch
 
 @click.group(hidden=True)
 def admin():
-    """admin [ add-repo | delete-repo ]"""
+    """admin [ onboard-repo | update-repo | delete-repo ]"""
     pass
 
+def parse_ioc_deployments(content):
+    # Initialize variables
+    facilities = {}
+    current_facility = None
+    current_master_release = None
+    
+    # ANSI escape code pattern
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+
+    # Read the file
+    for line in content.split('\n'):
+        # Strip ANSI codes first, then whitespace
+        line = ansi_escape.sub('', line).strip()
+        
+        # Skip empty lines
+        if not line:
+            continue
+        
+        # Check for facility line
+        # ex: Current versions on facility: LCLS
+        facility_match = re.match(r'Current versions on facility:\s*(\w+)', line)
+        if facility_match:
+            current_facility = facility_match.group(1).upper()
+            facilities[current_facility] = {
+                "facility": current_facility,
+                "tag": None,
+                "dependsOn": []
+            }
+            continue
+        
+        # Check for master release line
+        # ex: Current master release => l2MpsLN-R5-4-6
+        master_match = re.match(r'Current master release\s*=>\s*([\w\-\.]+)', line)
+        if master_match and current_facility:
+            current_master_release = master_match.group(1)
+            facilities[current_facility]["tag"] = current_master_release
+            continue
+        
+        # Check for IOC line
+        # ex: IOC: sioc-b084-mp05 => l2MpsLN-R3-14-0
+        ioc_match = re.match(r'IOC:\s*([\w\-]+)\s*=>\s*([\w\-\.]+)', line)
+        if ioc_match and current_facility:
+            ioc_name = ioc_match.group(1)
+            ioc_tag = ioc_match.group(2)
+            
+            facilities[current_facility]["dependsOn"].append({
+                "name": ioc_name,
+                "tag": ioc_tag
+            })
+    
+    # Remove empty facilities or those without IOCs
+    # Expanded version of the one-liner
+    filtered_facilities = {}
+    for facility_name, facility_data in facilities.items():
+        if facility_data["dependsOn"]:  # If the dependsOn list is not empty
+            filtered_facilities[facility_name] = facility_data
+
+    return filtered_facilities
+
+def add_initial_deployment(component: str, verbose: bool = False):
+    """Add an initial deployment configuration in the database (Useful for IOCS only at the moment) (CAUTION)"""
+
+    # The logic is done through the deployment controller,
+    # so if api ever changes, you don't need to update this function. 
+
+    # Grab deployment info from cram ls
+    click.echo("== ADBS == Adding initial deployment configuration")
+    click.echo("== ADBS == Running 'cram ls'...")
+
+    # TODO: TEMP
+    cram_output = """
+Current versions on facility: Dev 
+Current master release => 1.0.67
+IOC: sioc-b34-gtest01 => 1.0.67 (*)
+IOC: sioc-b34-gtest02 => 1.0.67 (*)
+"""
+    result = parse_ioc_deployments(cram_output)
+
+    # try:
+    #     # Run the command and capture output
+    #     cram_output = subprocess.run(
+    #         ['cram', 'ls'],
+    #         capture_output=True,
+    #         text=True,
+    #         check=True
+    #     )
+        
+    #     # Parse the cram output
+    #     # Transform the data and send to deployment controller to add to db
+    #     result = parse_ioc_deployments(cram_output.stdout)
+        
+    # except subprocess.CalledProcessError as e:
+    #     click.echo(f"== ADBS == Error running cram ls: {e.stderr}")
+    #     return
+
+    # Print each facility as a separate JSON object
+    for facility_name, facility_data in result.items():
+        print(f"Facility: {facility_name}")
+        print(json.dumps(facility_data, indent=2))
+        print("\n" + "-"*50 + "\n")
+    if (click.confirm("Does the above deployment info look correct")):
+        question = [
+        inquirer.List(
+            "deploymentType",
+            message="What type of deployment will this app use?",
+            choices=["ioc", "hla", "tools", "matlab", "pydm", "container"]
+            ),
+        ]
+        deployment_type = inquirer.prompt(question)['deploymentType']
+        for facility, facility_data in result.items():
+            data_to_write = {
+                "facility": facility,
+                "component_name": component,
+                "tag": facility_data['tag'],
+                "user": cli_configuration['github_uname'],
+                "type": deployment_type
+            }
+            if (deployment_type == 'ioc'):
+                data_to_write["ioc_list"] = facility_data['dependsOn']
+            request = Request(Component(component), Api.DEPLOYMENT)
+            request.set_endpoint(ApiEndpoints.DEPLOYMENT_INITIAL)
+            request.add_dict_to_payload(data_to_write)
+            request.put_request(log=verbose, msg="Add initial deployment for component in facility: " + facility)
+
+def add_repo(verbose: bool=False):
+    """Add a component and all its existing branches to the component database"""
+    request = Request(Component())
+
+    # === Lets make sure the admin is in a checked out copy of the repo,
+    # then we can parse the manifest and only question to ask is the automated test part
+    # 1) First check that user is in repo
+    if (not request.component.set_cur_dir_component()):
+        click.echo('fatal: not a git repository (or any of the parent directories)')
+        return
+    
+    # 2) Parse manifest
+    user_src_repo = request.component.git_get_top_dir()
+    manifest_filepath = user_src_repo + '/config.yaml'
+    manifest_data = request.component.parse_manifest(manifest_filepath)
+
+    # 3) Populate the payload
+    request.add_to_payload("name", manifest_data["repo"])
+    request.add_to_payload("description", manifest_data["description"])
+    request.add_to_payload("testingCriteria", manifest_data["testingCriteria"])
+    request.add_to_payload("approvalRule", manifest_data["approvalRule"])
+    organization = manifest_data["organization"]
+    request.add_to_payload("organization", organization)
+    issue_tracker = manifest_data["issueTracker"]
+    request.add_to_payload("issueTracker", issue_tracker)
+    if (issue_tracker != 'jira' and issue_tracker != 'github'): 
+        click.echo("== ADBS == issue tracker must be jira or github.")
+        return
+    if (issue_tracker == 'jira'):
+        request.add_to_payload("jiraProjectKey", manifest_data["jiraProjectKey"])
+
+    build_os = {"buildOs": manifest_data.get("environments")}
+    request.add_dict_to_payload(build_os)
+    request.add_to_payload("url", f"https://github.com/{organization}/{request.component.name}")
+    request.add_to_payload("ssh", f"git@github.com:{organization}/{request.component.name}.git")
+    if (not click.confirm("Do you want to add automated build and tests?")):
+        request.add_to_payload("skipBuildAndTests", True)
+    
+    request.set_endpoint(ApiEndpoints.COMPONENT)
+    request.post_request(verbose, msg="Add component to component database")
+
+    # Create another put request but to enable permissions for backend to receive events
+    request = Request(request.component)
+    request.set_endpoint(ApiEndpoints.COMPONENT_EVENT, 
+                         component_name=request.component.name,
+                         enable="true")
+    request.put_request(log=verbose, msg="Enable events for component")
+
+    # Add the main branch automatically
+    request = Request(request.component)
+    branches = request.component.git_get_branches()
+    if ("main" in branches):
+        main_branch = "main"
+    elif ("master" in branches):
+        main_branch = "master"
+    request.set_endpoint(ApiEndpoints.COMPONENT_BRANCH,
+                         component_name=request.component.name)
+    request.add_to_payload("type", "branch")
+    request.add_to_payload("branchName", main_branch)
+    request.put_request(log=verbose, msg="Add branch to component database")
+
 @admin.command()
-def configure_repo():
-    """Configure repo to integrate into software factory"""
+@click.pass_context
+@click.option("-v", "--verbose", is_flag=True, required=False, help="More detailed output")
+def onboard_repo(ctx, verbose: bool=False):
+    """Command to onboard a repo to software factory"""
+    click.confirm("Ensure you did a 'kinit' before continuing.")
+    testing_criteria = "all" # Default to all since functionality is not used yet
+    approval_rule = "all"
+
+    # Create the config.yaml =====================================================
     # Get user input
     at_top = click.confirm("Are you at the TOP level of your repo?")
     if (at_top):
@@ -156,81 +350,38 @@ ALARM_CONFIGS_TOP=/afs/slac/g/lcls/tools/AlarmConfigsTop
 
         # Remove RELEASE_SITE from .gitignore
         # Read all lines
-        with open('.gitignore', 'r') as file:
-            lines = file.readlines()
+        if os.path.exists('.gitignore'):
+            with open('.gitignore', 'r') as file:
+                lines = file.readlines()
 
-        # Remove the one line containing 'RELEASE_SITE'
-        filtered_lines = [line for line in lines if 'RELEASE_SITE' not in line.strip()]
+            # Remove the one line containing 'RELEASE_SITE'
+            filtered_lines = [line for line in lines if 'RELEASE_SITE' not in line.strip()]
 
-        # Write back
-        with open('.gitignore', 'w') as file:
-            file.writelines(filtered_lines)
+            # Write back
+            with open('.gitignore', 'w') as file:
+                file.writelines(filtered_lines)
 
-        click.echo(f"Successfully removed RELEASE_SITE from .gitignore")
-    
+            click.echo(f"Successfully removed RELEASE_SITE from .gitignore")
 
-@admin.command()
-@click.option("-v", "--verbose", is_flag=True, required=False, help="More detailed output")
-def add_repo(verbose: bool=False):
-    """Add a component to the component database"""
-    request = Request(Component())
+    add_repo(verbose)
+        
+    if (deployment_type == 'ioc'):
+        if (click.confirm("Is this an existing IOC application?")):
+            add_initial_deployment(repo_name, verbose)
+        else:
+            click.echo("No initial deployment configuration will be added since this is a NEW IOC application")        
 
-    # === Lets make sure the admin is in a checked out copy of the repo,
-    # then we can parse the manifest and only question to ask is the automated test part
-    # 1) First check that user is in repo
-    if (not request.component.set_cur_dir_component()):
-        click.echo('fatal: not a git repository (or any of the parent directories)')
-        return
-    
-    # 2) Parse manifest
-    user_src_repo = request.component.git_get_top_dir()
-    manifest_filepath = user_src_repo + '/config.yaml'
-    manifest_data = request.component.parse_manifest(manifest_filepath)
+    # Create software factory onboarding branch to push changes to
+    click.echo("== ADBS == Creating software factory onboard branch to push changes to")
+    ctx.invoke(
+        branch,
+        verbose=verbose
+    )
 
-    # 3) Populate the payload
-    request.add_to_payload("name", manifest_data["repo"])
-    request.add_to_payload("description", manifest_data["description"])
-    request.add_to_payload("testingCriteria", manifest_data["testingCriteria"])
-    request.add_to_payload("approvalRule", manifest_data["approvalRule"])
-    organization = manifest_data["organization"]
-    request.add_to_payload("organization", organization)
-    issue_tracker = manifest_data["issueTracker"]
-    request.add_to_payload("issueTracker", issue_tracker)
-    if (issue_tracker != 'jira' and issue_tracker != 'github'): 
-        click.echo("== ADBS == issue tracker must be jira or github.")
-        return
-    if (issue_tracker == 'jira'):
-        request.add_to_payload("jiraProjectKey", manifest_data["jiraProjectKey"])
-
-    build_os = {"buildOs": manifest_data.get("environments")}
-    request.add_dict_to_payload(build_os)
-    request.add_to_payload("url", f"https://github.com/{organization}/{request.component.name}")
-    request.add_to_payload("ssh", f"git@github.com:{organization}/{request.component.name}.git")
-    if (not click.confirm("Do you want to add automated build and tests?")):
-        request.add_to_payload("skipBuildAndTests", True)
-    
-    request.set_endpoint(ApiEndpoints.COMPONENT)
-    request.post_request(verbose, msg="Add component to component database")
-
-    # Create another put request but to enable permissions for backend to receive events
-    request = Request(request.component)
-    request.set_endpoint(ApiEndpoints.COMPONENT_EVENT, 
-                         component_name=request.component.name,
-                         enable="true")
-    request.put_request(log=verbose, msg="Enable events for component")
-
-    # Add the main branch automatically
-    request = Request(request.component)
-    branches = request.component.git_get_branches()
-    if ("main" in branches):
-        main_branch = "main"
-    elif ("master" in branches):
-        main_branch = "master"
-    request.set_endpoint(ApiEndpoints.COMPONENT_BRANCH,
-                         component_name=request.component.name)
-    request.add_to_payload("type", "branch")
-    request.add_to_payload("branchName", main_branch)
-    request.put_request(log=verbose, msg="Add branch to component database")
+    click.echo(f"**** Post Steps ****\n \
+1. Please add repo to github app in {org_name}.\n \
+2. Update RELEASE_NOTES.md file with new tag then commit changes and push\n \
+3. Create new pull request and merge changes")
 
 @admin.command()
 @click.option("-c", "--component", required=False, help="Component Name", prompt=INPUT_PREFIX + "Specify component name")
@@ -295,117 +446,11 @@ def delete_repo(component: str, verbose: bool):
                          enable="false")
     request.put_request(log=verbose, msg="Disable events for component")
 
-    # Request to add component to database
+    # Request to delete component from database
     request = Request(Component(component))
     component_id = request.get_component_from_db()['id']
     request.set_endpoint(ApiEndpoints.COMPONENT_ID,
                          id=component_id)
     request.delete_request(log=verbose, msg="Delete component from component database")
-
-def parse_ioc_deployments(file_path):
-    # Initialize variables
-    facilities = {}
-    current_facility = None
-    current_master_release = None
-    
-    # Read the file
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.strip()
-            
-            # Check for facility line
-            # ex: Current versions on facility: LCLS
-            facility_match = re.match(r'Current versions on facility: (\w+)', line)
-            if facility_match:
-                current_facility = facility_match.group(1).upper()
-                facilities[current_facility] = {
-                    "facility": current_facility,
-                    "tag": None,
-                    "dependsOn": []
-                }
-                continue
-            
-            # Check for master release line
-            # ex: Current master release => l2MpsLN-R5-4-6
-            master_match = re.match(r'Current master release => ([\w\-\.]+)', line)
-            if master_match and current_facility:
-                current_master_release = master_match.group(1)
-                facilities[current_facility]["tag"] = current_master_release
-                continue
-            
-            # Check for IOC line
-            # ex: IOC: sioc-b084-mp05 => l2MpsLN-R3-14-0
-            ioc_match = re.match(r'IOC: ([\w\-]+) => ([\w\-\.]+)', line)
-            if ioc_match and current_facility:
-                ioc_name = ioc_match.group(1)
-                ioc_tag = ioc_match.group(2)
-                
-                facilities[current_facility]["dependsOn"].append({
-                    "name": ioc_name,
-                    "tag": ioc_tag
-                })
-    
-    # Remove empty facilities or those without IOCs
-    # Expanded version of the one-liner
-    filtered_facilities = {}
-    for facility_name, facility_data in facilities.items():
-        if facility_data["dependsOn"]:  # If the dependsOn list is not empty
-            filtered_facilities[facility_name] = facility_data
-
-    return filtered_facilities
-
-@admin.command()
-@click.option("-c", "--component", required=False, help="Component Name", prompt=INPUT_PREFIX + "Specify component name")
-@click.option("-v", "--verbose", is_flag=True, required=False, help="More detailed output")
-def add_initial_deployment(component: str, verbose: bool):
-    """Add an initial deployment configuration in the database (Useful for IOCS only at the moment) (CAUTION)"""
-
-    # The logic is done through the deployment controller,
-    # so if api ever changes, you don't need to update this function. 
-
-    # Grab deployment info from cram ls
-    steps = """Instructions (Please read all first): 
-    Go to dev3 and run "cram ls | sed -e 's/\x1b\[[0-9;]*m//g' > cram_deployment.txt"
-    Then go to where you have access to bs cli and wherever you call
-    bs admin add-initial-deployment, add the cram_deployment.txt there
-    it'll be parsed, then added to software factory db, then you can delete the file.
-            """
-    click.echo(steps)
-    if (click.confirm("Have you completed the steps?")):
-        # Parse the cram output
-        # Transform the data and send to deployment controller to add to db
-        result = parse_ioc_deployments("cram_deployment.txt")
-    
-        # Print each facility as a separate JSON object
-        for facility_name, facility_data in result.items():
-            print(f"Facility: {facility_name}")
-            print(json.dumps(facility_data, indent=2))
-            print("\n" + "-"*50 + "\n")
-        if (click.confirm("Does the above deployment info look correct")):
-            question = [
-            inquirer.List(
-                "deploymentType",
-                message="What type of deployment will this app use?",
-                choices=["ioc", "hla", "tools", "matlab", "pydm", "container"]
-                ),
-            ]
-            deployment_type = inquirer.prompt(question)['deploymentType']
-            for facility, facility_data in result.items():
-                data_to_write = {
-                    "facility": facility,
-                    "component_name": component,
-                    "tag": facility_data['tag'],
-                    "user": cli_configuration['github_uname'],
-                    "type": deployment_type
-                }
-                if (deployment_type == 'ioc'):
-                    data_to_write["ioc_list"] = facility_data['dependsOn']
-                request = Request(Component(component), Api.DEPLOYMENT)
-                request.set_endpoint(ApiEndpoints.DEPLOYMENT_INITIAL)
-                request.add_dict_to_payload(data_to_write)
-                request.put_request(log=verbose, msg="Add initial deployment for component in facility: " + facility)
-    else:
-        click.echo("Please complete the instructions")
-
 
 
